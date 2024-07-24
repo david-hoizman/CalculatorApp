c2960bfc3a4b30e06e6a2de426e0a4de0223387a
david <mefdavid2024@gmail.com>
2024-07-24 12:51:42 +0300

    Added % button

--- components/ButtonCalc.js
+++ components/ButtonCalc.js
@@ -1,32 +1,44 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import styles from '../style/ButtonClac';
 
-
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
-import styles from '../style/ButtonClac'
-
-export default function ButtonCalc({ value, calc, setCalc }) {
-
-  const operators = ['+', '-', '/', '*', '%']
+const ButtonCalc = ({ value, calc, setCalc }) => {
+  const operators = ['+', '-', '/', '*', '%'];
 
   const handleClick = () => {
     if (operators.includes(value.val) && operators.includes(calc.charAt(calc.length - 1))) {
-      return
+      return;
     }
     if (value.val === 'C') {
-      setCalc('')
+      setCalc('');
     } else if (value.val === '=') {
-      setCalc(eval(calc)) // calculate the result
+      setCalc(eval(calc).toString()); // calculate the result
     } else if (value.val === '+/-') {
       const result = parseFloat(calc) * -1;
       setCalc(result.toString());
+    } else if (value.val === '%') {
+      if (calc.length === 0) {
+        return; // לא ניתן לבצע פעולה על מחרוזת ריקה
+      }
+
+      const lastIndex = calc.lastIndexOf(' ');
+      const lastNumber = calc.slice(lastIndex + 1);
+
+      if (isNaN(parseFloat(lastNumber))) {
+        return; // המספר האחרון בחישוב אינו חוקי
+      }
+
+      const percentage = parseFloat(lastNumber) / 100;
+      const newCalculation = calc.slice(0, lastIndex + 1) + percentage;
+      setCalc(newCalculation);
     } else {
-      setCalc(calc + value.val)
+      setCalc(calc + value.val);
     }
-  }
+  };
 
   return (
     <TouchableOpacity
-      style={value.type === "classic" ? styles.classsicButton : styles.specialButton}
+      style={value.type === 'classic' ? styles.classsicButton : styles.specialButton}
       onPress={handleClick}
     >
       <Text style={styles.buttonText}>{value.val}</Text>
@@ -34,56 +46,17 @@ export default function ButtonCalc({ value, calc, setCalc }) {
   );
 };
 
+export default ButtonCalc;
 
 
-// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-// import React from 'react'
-// import styles from '../style/ButtonClac'
-// import math from 'mathjs' // ספריית eval חלופית
 
-// export default function ButtonCalc({ value, calc, setCalc }) {
 
-//   const operators = ['+', '-', '/', '*', '%']
 
-//   const handleClick = () => {
-//     if (operators.includes(value.val) && operators.includes(calc.charAt(calc.length - 1))) {
-//       return
-//     }
-//     if (value.val === 'C') {
-//       setCalc('')
-//     } else if (value.val === '=') {
-//       try {
-//         setCalc(math.evaluate(calc)) // חישוב התוצאה באמצעות mathjs
-//       } catch (error) {
-//         console.error(error);
-//         setCalc('Error'); // הצגת הודעת שגיאה
-//       }
-//     } else if (value.val === '+/-') {
-//       const numbers = calc.split(/[+-\/*%]/);
-//       const lastNumber = parseFloat(numbers[numbers.length - 1]);
 
-//       if (lastNumber === 0) {
-//         return;
-//       }
 
-//       const reversedNumber = lastNumber * -1;
-//       const newCalc = numbers.slice(0, numbers.length - 1).join('') + reversedNumber.toString();
 
-//       setCalc(newCalc);
-//     } else {
-//       setCalc(calc + value.val)
-//     }
-//   }
 
-//   return (
-//     <TouchableOpacity
-//       style={value.type === "classic" ? styles.classsicButton : styles.specialButton}
-//       onPress={handleClick}
-//     >
-//       <Text style={styles.buttonText}>{value.val}</Text>
-//     </TouchableOpacity>
-//   );
-// };
+
 
 
  
