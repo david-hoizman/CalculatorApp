421c0d5e689d818f87ef0acd6488c54a4f209b0b
david <mefdavid2024@gmail.com>
2024-07-24 15:56:23 +0300

    clear commenst

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import Calculator from './components/Calculator';
-// import Container from './components/Container';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Container/>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-
-
 import { StatusBar } from 'expo-status-bar'; // Importing StatusBar component from Expo
 import { StyleSheet, Text, View } from 'react-native'; // Importing necessary components from React Native
 import Calculator from './components/Calculator'; // Importing Calculator component from local file
